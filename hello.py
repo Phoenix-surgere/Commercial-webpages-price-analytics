be92652ca7b57f1f388d75cedae591e717f8912f
Constantine <blackphoenix343@gmail.com>
2023-06-01 23:45:02 +0200

    initial file upload testing

---
+++ hello.py
@@ -0,0 +1,10 @@
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+print("fefe")
+
+x = np.linspace(0, 20,100)
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()
+
+url = "https://www.kdnuggets.com/2022/07/12-essential-vscode-extensions-data-science.html"
